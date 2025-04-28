a4d9cd308eb421e4661eba405a69c57e8e32cb8c
Patelpankaj1947 <patep9717@gmail.com>
2025-04-28 15:31:21 +0530

    hellol

---
+++ OneDrive/Desktop/Java Programs/Hello.java
@@ -0,0 +1,15 @@
+ public class Hello {
+  static void sum(){
+  System.out.println("hellorrrr");
+  }
+ public static void main(String[] args) {
+
+   sun();
+  
+
+        
+    }
+}
+ 
+    
+ 
---
+++ OneDrive/Desktop/Java Programs/hiii.java
@@ -0,0 +1,12 @@
+public class hiii {
+public static void main(String[] args) {
+   for(int i = 3;i<100;i++){
+    System.out.println("jay shree ram");
+   }
+    }
+}
+    
+
+    
+        
+    
